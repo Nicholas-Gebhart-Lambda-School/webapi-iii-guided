8fbbd917497e0051fe451af6807e056e0c00e784
unknown <nicholas.gebhart@gmail.com>
2019-10-30 10:34:43 -0700

    feat: add gatekeeper

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 const express = require("express"); // importing a CommonJS module
 const helmet = require("helmet");
+const morgan = require("morgan");
 const hubsRouter = require("./hubs/hubs-router.js");
 
 const server = express();
 
-const newLogger = (req, res, next) => {
-  const { url, ip, method } = req;
-  console.log(`
-  url: ${url}, 
-  ip: ${ip}, 
-  method: ${method}`);
-  next();
-};
+function gateKeeper(req, res, next) {
+  const password = req.headers.password;
+  !password
+    ? res.status(400).json({ message: "please provide a password" })
+    : password.toLowerCase() === "mellon"
+    ? next()
+    : res.status(401).json({ you: "cannot pass!!" });
+}
 
 server.use(helmet());
+server.use(gateKeeper);
 server.use(express.json());
-server.use(newLogger);
+server.use(morgan("dev"));
 
 server.use("/api/hubs", hubsRouter);
 
